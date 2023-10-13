7dcc12bdbf80102b1070276ca96e917429cc248a
hung-le-tpv-mti <hung.le.tpv@one-line.com>
2023-10-13 12:07:52 +0700

    v3-middle-ware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { PublishersModule } from './publishers/publishers.module';
+import { AuthMiddleware } from './shared/middleware/auth.middleware';
 
 @Module({
   imports: [BooksModule, PublishersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
